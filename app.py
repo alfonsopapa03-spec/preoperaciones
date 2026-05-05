be0674479f38d0a5c33def0c2cc5ab57abb0fd49
alfonsopapa03-spec <alfonsopapa03@gmail.com>
2026-05-05 10:40:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 )
 
 # ==================== CREDENCIALES ====================
-SUPABASE_DB_URL = "postgresql://postgres.ogfenizdijcboekqhuhd:Conejito200%24@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
+SUPABASE_DB_URL = "postgresql://postgres:Conejito200$@db.ogfenizdijcboekqhuhd.supabase.co:5432/postgres"
 # ==================== CATÁLOGO DE MÁQUINAS ====================
 # ⚙️ CAMBIA AQUÍ LOS NOMBRES REALES DE TUS 11 MÁQUINAS
 MAQUINAS = [
